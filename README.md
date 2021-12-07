a8ca35b31610557db0d55f675c96a6f0af89e7e4
Abed-Karim <21439026@student.uwl.ac.uk>
2021-12-07 10:22:39 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-dc1oyg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-dc1oyg)
---
+++ index.js
@@ -0,0 +1,14 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+var x = 3;
+var y = 1.1;
+print(x + y);
+print(x * y);
+print((x+y)*(x-y));
+var z = 9;
+print(Math.sqrt(z));
+print(Math.abs(y/x));
+function print(value){
+console.log(value);
+}
